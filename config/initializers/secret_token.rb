9d8e5d750d5e94f813880093a29dc18bbf9f6170
sipes23 <pete@pluteopleno.com>
2011-02-18 20:00:03 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inuktitut::Application.config.secret_token = '91e499b91af305bb7fe728f4e58a11b3f6fbce054e2a7265578530fa3281cd2af8b40e165b2bba21082367c86dc51ec9f3c47c173d992dfc7108afb08bc56026'
